8a4534b07762011237703917649850d37cc1c82b
dimonioi4 <dimonioi4@yandex.ru>
2021-05-04 23:15:29 +0300

    update test

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -9,8 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginPageTest {
 
+
         @Test
-        public void avtorization (String[] args) {
+        public void avtorization () {
             var playwright = Playwright.create();
 
             var browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
@@ -24,15 +25,14 @@ public class LoginPageTest {
             page.fill("#input-22", "password");
             page.click("button.v-btn:nth-child(2) > span:nth-child(1)");
 
-
-            String text = page.innerText(".selected");
+            String text = page.innerText(".toasted");
             assertEquals("Неудачная попытка авторизоваться", text);
 
             browser.close();
         }
 
     @Test
-            public void registration (String[] args) {
+            public void registration () {
     var playwright = Playwright.create();
 
     var browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
@@ -44,23 +44,17 @@ public class LoginPageTest {
             page.navigate("https://dev.lk.tr-line.ru/sign-in");
 
             page.click("a.v-btn:nth-child(3) > span:nth-child(1)");
-            page.fill("#email-input", "ivan@yandex.ru");
-            page.fill("#input-143", "88005553535");
             page.click("button.v-btn > span:nth-child(1)");
-            page.fill("#input-163", "Иванов");
-            page.fill("#input-166", "Иван");
-            page.fill("#input-169", "Иванович");
-            page.click(".v-btn--is-elevated > span:nth-child(1)");
 
-        String text = page.innerText(".selected");
-        assertEquals("Пользователь успешно зарегистрирован", text);
+        String text = page.innerText(".v-messages__message");
+        assertEquals("Необходимо указать e-mail", text);
 
 
     browser.close();
 }
 
 @Test
-    public void recoverPassword  (String[] args) {
+    public void recoverPassword  () {
         var playwright = Playwright.create();
 
         var browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
@@ -73,7 +67,7 @@ public class LoginPageTest {
         page.click("a.v-btn:nth-child(1) > span:nth-child(1)");
         page.fill("#input-38", "ivan@yandex.ru");
         page.click(".v-btn");
-        String text = page.innerText(".selected");
+        String text = page.innerText(".toasted");
         assertEquals("Пользователя с такими данными не существует.", text);
 
         browser.close();
